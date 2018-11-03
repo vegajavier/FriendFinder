9377fec651600d7341cad6518573fc67629bd79e
Javier <vegajavier86@gmail.com>
2018-11-03 01:45:10 -0500

    button works, more javascript attempts

--- App/routing/apiRoutes.js
+++ App/routing/apiRoutes.js
@@ -1,4 +1,4 @@
-let friends = require('../data/friends');
+let friends = require('../data/friends.js');
 let path = require("path");
 
 module.exports = function(app) {
@@ -6,21 +6,36 @@ module.exports = function(app) {
         res.json(friends);
     });
     //API post request
-    app.post('/api/friends', function(req,res){
+    app.post("/api/friends", function(req, res) {
 
-    //user inputs
-    let user = req.body;
-    let userInfo = user.results;
+        let bff = {
+            name: "",
+            photo: "",
+            difference: 100
 
-    let friendMatch = "";
-    let friendImage = "";
-    let difference = [];
+        };
 
-    let bffMath = userInfo.reduce((a,b) => a  + parseInt(b), 0);
-    let bffScore = parseInt(bffMath);
+        let userInfo = req.body;
+        let userResults = userInput.results;
 
+        /////Loop friends.js arr
+        for (var i = 0; i < friends.length; i++) {
+            let resultsDifference = 0;
 
+            for (var j = 0; j < friends[i].results[j]; j++) {
+                resultsDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].results[j]));
 
-        
-    })
+                if (resultsDifference <= bff.difference) {
+                    bff.name = friends[i].name;
+                    bff.photo = friends[i].photo;
+                    bff.difference = resultsDifference
+                }
+            }
+        }
+        //Save new user to database
+        friends.push(userInfo);
+
+        res.json(bff);
+    });
+
 }
