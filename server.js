7a95822bfb4e99e4b5e754ea736418a1be9b0163
Javier <vegajavier86@gmail.com>
2018-11-02 22:20:40 -0500

    install body parser

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let express = require ('express');
+//bodyParser object exposes various factories to create middlewares. All middlewares will populate the req.body
+let bodyParser = require("body-parser");
 
-//description
 //Put a description in it. It's a string. This helps people discover your package, as it's listed in npm search.
 let path = require('path');
 
